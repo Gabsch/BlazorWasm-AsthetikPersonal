097ec29de908f5a8a388d0e2e1b1a17b039a6b37
Gabriel David Schneider <gabriel.d.schneider@outlook.com>
2020-10-28 01:01:08 +0100

    Projektdateien hinzufügen.

---
+++ Models/BookingItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gabsch.AsthetikPersonal.Models
+{
+    public class BookingItem
+    {
+        public string DisplayName { get; set; }
+        public string Description { get; set; }
+        public string Time{ get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Gabsch.AsthetikPersonal
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+
+            ConfigureServices(builder);
+
+            await builder.Build().RunAsync();
+        }
+
+        private static void ConfigureServices(WebAssemblyHostBuilder builder)
+        {
+            var services = builder.Services;
+            services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+        }
+    }
+}
