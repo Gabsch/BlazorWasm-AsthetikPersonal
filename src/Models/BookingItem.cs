ba516873c7b2ccdaeb9bdca9b1521c5ac88a8c74
Gabriel David Schneider <gabriel.d.schneider@outlook.com>
2021-01-10 03:59:08 +0100

    Cardarray to use input-data

--- src/Models/BookingItem.cs
+++ src/Models/BookingItem.cs
@@ -8,8 +8,11 @@ namespace Gabsch.AsthetikPersonal.Models
     public class BookingItem
     {
         public string DisplayName { get; set; }
+        public string AlternateName { get; set; }
         public string Description { get; set; }
         public string Time{ get; set; }
         public decimal Price { get; set; }
+        public int Row { get; set; }
+        public IList<string> BulletPoints { get; set; }
     }
 }
