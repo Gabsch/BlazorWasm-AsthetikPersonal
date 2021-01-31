0a9ed0faaeb78681240b30093f324d70f0da01ee
Gabsch <gabriel.d.schneider@outlook.com>
2021-01-31 13:42:39 +0000

    Deploying to gh-pages from  @ bff2f3c6c7b9b2f4162500064994470d6aab82fe 🚀

---
+++ _content/DevExpress.Blazor.AnchorUtils/anchor-utils.js
@@ -0,0 +1,26 @@
+export function scrollToAnchor(anchor) {
+    var selector = anchor || document.location.hash;
+    if (selector && selector.length > 1) {
+        var element = document.querySelector(selector);
+        if (element) {
+            var y = element.getBoundingClientRect().top + window.pageYOffset;
+            /*The following code line updates a vertical scroll bar's offset for a standard Blazor Visual Studio template.
+            Update the code to get an offset that is suitable for your application.*/
+            //y -= document.querySelector(".main").offsetHeight;
+            window.scroll(0, y);
+        }
+    }
+    else
+        window.scroll(0, 0);
+}
+
+export function checkScrollSpy() {
+    var scrollSpys = [].slice.call(document.querySelectorAll('[data-spy="scroll"]'));
+    var scrollSpysLength = scrollSpys.length;
+
+    for (var i = scrollSpysLength; i--;) {
+        var $spy = $(scrollSpys[i]);
+
+        $.fn['scrollspy'].call($spy, $spy.data());
+    }
+}
