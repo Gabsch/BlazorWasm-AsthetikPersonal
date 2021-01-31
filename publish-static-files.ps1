36f51ce9eec4851bacd7a10446477e0f2cd360fe
Gabriel David Schneider <gabriel.d.schneider@outlook.com>
2021-01-31 14:40:15 +0100

    Fix project path to respect new path

--- publish-static-files.ps1
+++ publish-static-files.ps1
@@ -1,5 +1,5 @@
 # publish the app
-dotnet publish -c Release -o output Gabsch.AsthetikPersonal
+dotnet publish -c Release -o output src\Gabsch.AsthetikPersonal
 
 # Set the render output (use $PSScriptRoot instead of $pwd inside of *.ps1 script files)
 $env:RenderOutputDirectory="$PSScriptRoot/output/wwwroot"
